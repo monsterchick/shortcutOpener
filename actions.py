755ec8b88760c5d4c94b5202c9e4ac2a6ec4b768
monsterchick <38910509+monsterchick@users.noreply.github.com>
2023-04-02 01:30:03 -0700

    Update actions.py

--- actions.py
+++ actions.py
@@ -3,7 +3,7 @@ import pyautogui
 
 def find_icon(image):
     try:
-        img_pos_box = pyautogui.locateOnScreen(image)
+        img_pos_box = pyautogui.locateOnScreen(image, confidence=0.7)
         # print(img_pos_box)
         img_center_point = pyautogui.center(img_pos_box)
         # print(img_center_point)
@@ -11,5 +11,3 @@ def find_icon(image):
         pyautogui.doubleClick(duration=1)
     except TypeError:
         print('{i} did not match. Please try again.'.format(i=image.split('\\')[-1]))
-
-
